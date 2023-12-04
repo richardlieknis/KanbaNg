43033732661db9b4ce97d2a608c7703f38c183d3
Richard Lieknis <richard.lieknis@web.de>
2023-12-04 21:20:50 +0100

    sign up forms & form-error implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login/login-page/login-page.component';
@@ -21,7 +22,10 @@ import { ResetPwComponent } from './pages/login/reset-pw/reset-pw.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/sign-up/sign-up.component.ts
+++ src/app/pages/login/sign-up/sign-up.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  minNameLength = 5;
+  minPasswordLength = 5;
 
+  signupForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(this.minNameLength)]),
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(this.minPasswordLength)]),
+  });
+
+  onSubmit() {
+    console.log(this.signupForm.valid);
+    console.log(this.signupForm.value);
+  }
 }
