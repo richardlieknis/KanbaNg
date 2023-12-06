cb912e3c856f3b0811f05a8889c22694dc9fe1ea
Richard Lieknis <richard.lieknis@web.de>
2023-12-06 20:16:53 +0100

    login form added

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor() { }
+
+  signinForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
+
+  onSubmit() {
+    console.log(this.signinForm.value);
+  }
 }
