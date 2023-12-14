5440735d9df9a9cb4667966a88392703feaa1f10
Richard Lieknis <richard.lieknis@web.de>
2023-12-14 21:44:13 +0100

    add task expanded, select animation

--- src/app/pages/home/add-task/add-task.component.ts
+++ src/app/pages/home/add-task/add-task.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+  public categoryDropdown: boolean = false;
+  public assigneeDropdown: boolean = false;
 
+  constructor() { }
+
+  toggleCategoryDropdown() {
+    this.categoryDropdown = !this.categoryDropdown;
+  }
+
+  toggleAssigneeDropdown() {
+    this.assigneeDropdown = !this.assigneeDropdown;
+  }
 }
