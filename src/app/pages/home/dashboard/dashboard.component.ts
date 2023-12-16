68fd9cf39b40e0c185ec6f2a15c10c0e73cb1cab
Richard Lieknis <richard.lieknis@web.de>
2023-12-16 20:25:48 +0100

    time based greetings added

--- src/app/pages/home/dashboard/dashboard.component.ts
+++ src/app/pages/home/dashboard/dashboard.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public greetings = 'Hello, ';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.setCurrentTimeGreeting();
+  }
+
+
+  private setCurrentTimeGreeting() {
+    const currentTime = new Date().getHours();
+    if (currentTime < 12) {
+      this.greetings = 'Good Morning, ';
+    } else if (currentTime < 18) {
+      this.greetings = 'Good Afternoon, ';
+    } else {
+      this.greetings = 'Good Evening, ';
+    }
+  }
 }
