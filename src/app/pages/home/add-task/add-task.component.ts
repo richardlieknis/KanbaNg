fb4aa5cc8ce69390b10b3b5bd1e560f5ce0c2781
Richard Lieknis <richard.lieknis@web.de>
2023-12-16 15:46:00 +0100

    add-task adjustments

--- src/app/pages/home/add-task/add-task.component.ts
+++ src/app/pages/home/add-task/add-task.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit {
   public categoryDropdown: boolean = false;
   public assigneeDropdown: boolean = false;
-  public selected = 'low';
+  public currentDate: string = '';
+  public selected = '';
 
   constructor() { }
+
+  ngOnInit() {
+    this.setCurrentDate();
+  }
 
   toggleCategoryDropdown() {
     this.categoryDropdown = !this.categoryDropdown;
@@ -22,5 +27,13 @@ export class AddTaskComponent {
 
   changePriority(priority: string) {
     this.selected = priority;
+  }
+
+  setCurrentDate() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    this.currentDate = `${year}-${month}-${day}`;
   }
 }
