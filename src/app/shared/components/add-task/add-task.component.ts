c044d06d574fa31f7e14ded99c3e4640781042e4
Richard Lieknis <richard.lieknis@web.de>
2023-12-17 12:24:16 +0100

    new add-task overlay added

---
+++ src/app/shared/components/add-task/add-task.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-comp',
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.scss'
+})
+export class AddTaskCompComponent implements OnInit {
+  public categoryDropdown: boolean = false;
+  public assigneeDropdown: boolean = false;
+  public currentDate: string = '';
+  public selected = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.setCurrentDate();
+  }
+
+  toggleCategoryDropdown() {
+    this.categoryDropdown = !this.categoryDropdown;
+  }
+
+  toggleAssigneeDropdown() {
+    this.assigneeDropdown = !this.assigneeDropdown;
+  }
+
+  changePriority(priority: string) {
+    this.selected = priority;
+  }
+
+  setCurrentDate() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    this.currentDate = `${year}-${month}-${day}`;
+  }
+}
