f3f44883d784f21ab28187337a238316573df2ec
Richard Lieknis <richard.lieknis@web.de>
2023-12-17 20:09:36 +0100

    overlay animation fix

--- src/app/shared/components/overlay/overlay.component.ts
+++ src/app/shared/components/overlay/overlay.component.ts
@@ -3,6 +3,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { SnackbarService } from '../../services/snackbar.service';
 import { OverlayService } from '../../services/overlay.service';
 import { Subscription } from 'rxjs';
+import e from 'express';
 
 @Component({
   selector: 'app-overlay',
@@ -25,7 +26,7 @@ import { Subscription } from 'rxjs';
 })
 export class OverlayComponent implements OnInit, OnDestroy {
   public show = false;
-  public backdrop = false;
+  public animIsRunning = false;
   public component: string | null = null;
   private overlaySub: Subscription = new Subscription();
 
@@ -39,11 +40,18 @@ export class OverlayComponent implements OnInit, OnDestroy {
       .subscribe((state) => {
         this.component = state.component;
         this.show = state.show;
-        this.backdrop = state.show;
+        this.animIsRunning = true;
       });
   }
 
   ngOnDestroy() {
     this.overlaySub.unsubscribe();
+  }
+
+  test(event: any) {
+    if (event.toState === 'void') {
+      this.animIsRunning = false;
+    }
+    // event.stopPropagation();
   }
 }
