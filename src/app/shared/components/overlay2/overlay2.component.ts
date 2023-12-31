dba30530fc0da56a145903189d01d2a0a2ec1d9a
Richard Lieknis <richard.lieknis@web.de>
2023-12-31 19:07:19 +0100

    second overlay type working

--- src/app/shared/components/overlay2/overlay2.component.ts
+++ src/app/shared/components/overlay2/overlay2.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SnackbarService } from '../../services/snackbar.service';
+import { OverlayService } from '../../services/overlay.service';
 
 @Component({
   selector: 'app-overlay2',
   templateUrl: './overlay2.component.html',
-  styleUrl: './overlay2.component.scss'
-})
-export class Overlay2Component {
+  styleUrl: './overlay2.component.scss',
+  animations: [
+    trigger('state', [
+      transition(':enter', [
+        style({ left: '100%', opacity: 0 }),
+        animate('300ms ease-in', style({
+          left: '30%',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ left: '100%', opacity: 1 }))
+      ])
+    ]),
+    // BACKDROP ANIMATION
+    trigger('backdrop', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-in', style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0 }))
+      ])
+    ]),
 
+  ]
+})
+export class Overlay2Component implements OnInit, OnDestroy {
+  public show = false;
+  public animIsRunning = false;
+  public component: string | null = null;
+  public title: string = '';
+  public subtitle: string = '';
+  private overlaySub: Subscription = new Subscription();
+
+  constructor(
+    private snackbarService: SnackbarService,
+    public overlayService: OverlayService,
+  ) { }
+
+  ngOnInit() {
+    this.overlayService.overlay2State
+      .subscribe((state) => {
+        this.component = state.component;
+        this.subtitle = state.subtitle || '';
+        this.show = state.show;
+        this.animIsRunning = true;
+        this.setTitle(state.component);
+      });
+  }
+
+  ngOnDestroy() {
+    this.overlaySub.unsubscribe();
+  }
+
+  /**
+   * set animation state to false when animation is done
+   * @param event - state of animation
+   */
+  overlayBoxAnimation(event: any) {
+    if (event.toState === 'void') {
+      this.animIsRunning = false;
+    }
+  }
+
+  setTitle(title: string) {
+    title = title?.replace(/-/g, ' ');
+    this.title = title;
+  }
 }
--- src/app/shared/services/overlay.service.ts
+++ src/app/shared/services/overlay.service.ts
@@ -6,27 +6,51 @@ import { Subject } from 'rxjs';
 })
 export class OverlayService {
   private overlaySubject = new Subject<any>();
+  private overlay2Subject = new Subject<any>();
   public overlayState = this.overlaySubject.asObservable();
+  public overlay2State = this.overlay2Subject.asObservable();
 
-  /** Its a function to show overlay
-   *  It takes a parameter component as string and optional subtitle as string
+  private lastOverlay: string | null = null;
+
+  /** Show overlay - 2 types: standard and overlay with banner
    * @param component as string, component name to show in overlay
+   * @param type as string, type of overlay to show
+   * @param subtitle as string, subtitle to show in overlay
    */
-  show(component: string, subtitle?: string) {
-    this.overlaySubject.next({
-      show: true,
-      component,
-      subtitle
-    });
+  show(component: string, type?: string, subtitle?: string) {
+    if (type === 'standard') {
+      this.lastOverlay = 'standard';
+      this.overlaySubject.next({
+        show: true,
+        component,
+        subtitle
+      });
+    } else {
+      this.lastOverlay = null;
+      this.overlay2Subject.next({
+        show: true,
+        component,
+        subtitle
+      });
+    }
   }
 
-  /** Its a function to hide overlay */
+  /** close overlay */
   hide() {
-    this.overlaySubject.next({
-      show: false,
-      component: null
-    });
+    if (this.lastOverlay === 'standard') {
+      this.overlaySubject.next({
+        show: false,
+        component: null
+      });
+    } else {
+      this.overlay2Subject.next({
+        show: false,
+        component: null
+      });
+    }
   }
+
+
 
   constructor() { }
 }
