e91d61b5532c2acfb60f2251c38c9012cb420af1
Richard Lieknis <richard.lieknis@web.de>
2024-01-01 16:00:58 +0100

    contact layout added

--- src/app/shared/components/contact/contact.component.ts
+++ src/app/shared/components/contact/contact.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  @Input() type: string = 'edit';
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setFormType(this.type);
+  }
+
+  setFormType(type: string): void {
+    this.type = type;
+  }
 }
