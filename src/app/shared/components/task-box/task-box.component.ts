1c6d38e826125240caa10261f49523e18cf74ebe
Richard Lieknis <richard.lieknis@web.de>
2024-01-05 22:38:55 +0100

    task box continued

--- src/app/shared/components/task-box/task-box.component.ts
+++ src/app/shared/components/task-box/task-box.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FetchSqlService } from '../../services/fetch-sql.service';
+import { get } from 'http';
 
 @Component({
   selector: 'app-task-box',
@@ -9,13 +10,27 @@ import { FetchSqlService } from '../../services/fetch-sql.service';
 export class TaskBoxComponent implements OnInit {
   @Input() task: any = {};
 
+  public displayedAssignees: number = 3;
+
   public categories: any[] = [];
+  public contacts: any[] = [];
+  public assignees: [] = [];
 
   constructor(
     private sql: FetchSqlService,
   ) { }
 
   ngOnInit(): void {
+    this.createCategoryDictionary();
+    this.createAssigneeDictionary();
+
+  }
+
+  getAssignedContacts() {
+
+  }
+
+  createCategoryDictionary() {
     this.sql.getCategories().subscribe((data: any) => {
       this.categories = data.categories.reduce((acc: any, category: any) => {
         acc[category.category_id] = category;
@@ -24,19 +39,28 @@ export class TaskBoxComponent implements OnInit {
     });
   }
 
-  /**
-   * get category name by id
-   * @param categoryId 
-   */
+  createAssigneeDictionary() {
+    this.sql.getContacts().subscribe((data: any) => {
+      this.contacts = data.contacts.reduce((acc: any, contact: any) => {
+        acc[contact.contact_id] = contact;
+        return acc;
+      }, {});
+    });
+  }
+
   getCategoryName(categoryId: any) {
     return this.categories[categoryId]?.name;
   }
 
-  /**
-   * get category color by id
-   * @param categoryId 
-   */
   getCategoryColor(categoryId: any) {
     return this.categories[categoryId]?.color;
+  }
+
+  getAssigneeName(assigneeId: any) {
+    return this.contacts[assigneeId]?.name;
+  }
+
+  getAssigneeImage(assigneeId: any) {
+    return this.contacts[assigneeId]?.image;
   }
 }
