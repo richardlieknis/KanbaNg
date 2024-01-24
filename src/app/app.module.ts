5a5d84678d95766934159edf82fbbe1a26ca1518
Richard Lieknis <richard.lieknis@web.de>
2024-01-24 22:26:13 +0100

    login api changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login/login-page/login-page.component';
 import { ForgotPwComponent } from './pages/login/forgot-pw/forgot-pw.component';
 import { LoginComponent } from './pages/login/login/login.component';
 import { SignUpComponent } from './pages/login/sign-up/sign-up.component';
 import { ResetPwComponent } from './pages/login/reset-pw/reset-pw.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SnackbarComponent } from './shared/components/snackbar/snackbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './pages/home/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home/home.component';
 import { BoardComponent } from './pages/home/board/board.component';
@@ -20,7 +24,6 @@ import { ContactsComponent } from './pages/home/contacts/contacts.component';
 import { LegalNoticeComponent } from './pages/home/legal-notice/legal-notice.component';
 import { TaskBoxComponent } from './shared/components/task-box/task-box.component';
 import { HelpComponent } from './pages/home/help/help.component';
-import { CommonModule } from '@angular/common';
 import { AddTaskCompComponent } from './shared/components/add-task/add-task.component';
 import { OverlayComponent } from './shared/components/overlay/overlay.component';
 import { ProfileComponent } from './shared/components/profile/profile.component';
@@ -28,7 +31,6 @@ import { Overlay2Component } from './shared/components/overlay2/overlay2.compone
 import { ContactComponent } from './shared/components/contact/contact.component';
 import { ShowTaskComponent } from './shared/components/show-task/show-task.component';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DialogComponent } from './shared/components/dialog/dialog.component';
 
 @NgModule({
@@ -63,6 +65,7 @@ import { DialogComponent } from './shared/components/dialog/dialog.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    HttpClientXsrfModule,
     BrowserAnimationsModule,
     CommonModule,
     DragDropModule,
--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,20 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SnackbarService } from '../../../shared/services/snackbar.service';
 import { Router } from '@angular/router';
+import { CsrfTokenService } from '../../../shared/services/csrf-token.service';
+import e from 'express';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private backendUrl = 'http://localhost:8000/api/';
 
   constructor(
     private http: HttpClient,
     private snackbar: SnackbarService,
-    private router: Router
+    private router: Router,
+    private csrfTokenService: CsrfTokenService
   ) { }
 
   signinForm: FormGroup = new FormGroup({
@@ -22,18 +26,37 @@ export class LoginComponent {
     password: new FormControl('', [Validators.required, Validators.minLength(5)])
   });
 
+  ngOnInit() {
+    // console.log(this.getAccessToken());
+  }
+
+
+  // onSubmit() {
+  //   this.http.post('http://localhost/backend/verify_user.php', this.signinForm.value)
+  //     .subscribe((result: any) => {
+  //       if (result.status === 'success') {
+  //         this.snackbar.show(result.message, 'success');
+  //         setTimeout(() => {
+  //           this.router.navigate(['/home']);
+  //         }, 1000);
+  //       } else {
+  //         this.snackbar.show(result.message, 'error');
+  //       }
+  //     });
+  // }
 
   onSubmit() {
-    this.http.post('http://localhost/backend/verify_user.php', this.signinForm.value)
-      .subscribe((result: any) => {
-        if (result.status === 'success') {
-          this.snackbar.show(result.message, 'success');
-          setTimeout(() => {
-            this.router.navigate(['/home']);
-          }, 1000);
-        } else {
-          this.snackbar.show(result.message, 'error');
-        }
-      });
+    const http$ = this.http.post(this.backendUrl + 'login', this.signinForm.value);
+    http$.subscribe({
+      next: (result: any) => {
+        this.snackbar.show(result.message, 'success');
+        setTimeout(() => {
+          this.router.navigate(['/home']);
+        }, 1000);
+      },
+      error: (error: any) => {
+        this.snackbar.show(error.error.detail, 'error');
+      }
+    });
   }
 }
