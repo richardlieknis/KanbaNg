df60f6300174571111d07a7ed5768fa0112351c9
Richard Lieknis <richard.lieknis@web.de>
2024-01-29 18:07:21 +0100

    auth guard

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,5 +1,23 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  let userAccess = true;
+
+  authService.isLoggedIn$.subscribe({
+    next: (result: any) => {
+      if (!result) {
+        userAccess = result;
+        router.navigate(['/login']);
+      } else {
+        userAccess = result;
+      }
+    }
+  });
+
+  return userAccess;
 };
