44670c4332be7b7233f3d107ca49cc2c91d12b3e
Richard Lieknis <richard.lieknis@web.de>
2024-02-03 00:12:55 +0100

    complete password reset

--- src/app/pages/login/reset-pw/reset-pw.component.ts
+++ src/app/pages/login/reset-pw/reset-pw.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../shared/services/auth.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-pw',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './reset-pw.component.scss'
 })
 export class ResetPwComponent {
+  private token: any = '';
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+  ) { }
 
+  passwordForm: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    password2: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  });
+
+  ngOnInit() {
+    this.token = this.route.snapshot.paramMap.get('token')?.replace(/-/g, '.');
+  }
+
+  onSubmit() {
+    this.authService.changePassword({ token: this.token, password: this.passwordForm.value });
+  }
 }
