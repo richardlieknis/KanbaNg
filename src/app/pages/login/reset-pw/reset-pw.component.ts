9a911bf51855475af5f5616b29a0c59751030a11
Richard Lieknis <richard.lieknis@web.de>
2024-03-05 19:31:54 +0100

    reset password send to mail

--- src/app/pages/login/reset-pw/reset-pw.component.ts
+++ src/app/pages/login/reset-pw/reset-pw.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AuthService } from '../../../shared/services/auth.service';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-reset-pw',
@@ -9,22 +10,38 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './reset-pw.component.scss'
 })
 export class ResetPwComponent {
+  passwordForm: FormGroup;
+
   private token: any = '';
+
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-  ) { }
+  ) {
+    this.passwordForm = new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      password2: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    },
+      {
+        validators: this.passwordMatchValidator,
 
-  passwordForm: FormGroup = new FormGroup({
-    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    password2: new FormControl('', [Validators.required, Validators.minLength(5)]),
-  });
+      });
+  }
+
 
   ngOnInit() {
-    this.token = this.route.snapshot.paramMap.get('token')?.replace(/-/g, '.');
+    this.token = this.route.snapshot.paramMap.get('token')?.replace(/,/g, '.');
   }
 
   onSubmit() {
     this.authService.changePassword({ token: this.token, password: this.passwordForm.value });
+  }
+
+  // compare passwords
+  passwordMatchValidator(control: AbstractControl) {
+    return control.get('password')?.value ===
+      control.get('password2')?.value
+      ? null
+      : { mismatch: true };
   }
 }
